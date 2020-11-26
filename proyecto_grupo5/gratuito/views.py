e8f1696c6dd973292b5c810cef89c7c3f0303678
ALBERTO <AFLLM75@GMAIL.COM>
2020-11-26 19:35:08 +0100

    Select Districtes/Barris

--- proyecto_grupo5/gratuito/views.py
+++ proyecto_grupo5/gratuito/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import psycopg2.extras
 # Create your views here.
-"""
+
 def wifi(request):
     conn = psycopg2.connect(dbname="wifi_db",
                             user="grupo5_user",
                             password="patata")
     with open("debug.log", "a+") as debug_file:
-    print("Funciona!", file=debug_file)
+        print("Funciona!", file=debug_file)
     return HttpResponse('Funciona!')
-"""
+
 def select(request):
     conn = psycopg2.connect(dbname="wifi_db",
                             user="grupo5_user",
@@ -44,23 +44,27 @@ def insert(request):
     conn.close()
     return HttpResponse("Insertado")
 
-
 def prueba(request):
-    conn = psycopg2.connect(dbname="wifi_db",
+    iddistricte = request.GET.get('get_iddistricte', default=None)
+    #print(iddistricte)
+    if iddistricte is not None:
+        conn = psycopg2.connect(dbname="wifi_db",
                             user="grupo5_user",
                             password="patata")
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    iddistricte= request.GET.get('get_iddistricte', default='%')
-    with open("debug.log", "w") as debug_file:
-        print(f"SELECT * FROM barris WHERE idDistricte = '{iddistricte}';", file=debug_file)
-    if iddistricte == 'Todas':
-        iddistricte = '%'
-    cursor.execute(F"SELECT * FROM barris WHERE idDistricte = '{iddistricte}';")
-    result = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    params = {'barris': result}
-    return render(request, 'wifi.html', params)
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        """
+        with open("debug.log", "w") as debug_file:
+            print(f"SELECT * FROM barris WHERE idDistricte = '{iddistricte}';", file=debug_file)
+        """
+        cursor.execute(f"SELECT nom_barri FROM barris WHERE idDistricte = %s",(iddistricte,))
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        params = {'barris': result}
+        return render(request, 'wifi.html', params)
+    else:
+        #print('he entrado en else')
+        return render(request, 'wifi.html')
 
 def home_page(request):
-   return render(request,'wifi.html')
+   return render(request, 'wifi.html')
